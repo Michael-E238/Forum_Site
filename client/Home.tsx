7b7dcbc49e78bee3ff3a784b6e80d9791d3dd89f
delta_black5 <matt@kmgks.com>
2025-03-20 21:14:18 -0500

    home page skeleton built march 20

---
+++ client/Home.tsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import {Button, Row, Col, Card, Form, Container} from 'react-bootstrap';
+
+const Home: React.FC= () => {
+return (
+<><header style={{ paddingLeft: 0 }}></header>
+
+<div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+                    <div className='d-flex justify-content-center align-items-center h-100'>
+                        <div className='text-white'>
+                            <div className="contact">
+                                <header className="contact-header">
+                                    <Container>
+                                        <Row>
+                                            <Col>
+                                                <Card.Body>
+                                                    <Card.Title>
+                                                        <h1 className="titel">Welcome To The Olc School Internet</h1>
+                                                    </Card.Title>
+                                                    </Card.Body>
+                                                </Col>
+                                            </Row>
+                                        </Container>
+                                    </header>
+                                </div>
+                            </div>
+                        </div>
+
+            <div className="container">
+            <div className="row">
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+
+                // Force next columns to break to new line at md breakpoint and up
+                <div className="w-100 d-none d-md-block"></div>
+
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+
+                <div className="w-100 d-none d-md-block"></div>
+
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+
+            </div>
+            </div>
+</div></> 
+
+
+);
+};
+export default Home;
