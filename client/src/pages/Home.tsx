215429d955f208c5473ab95e2d2441501b57ee5b
Micahel-E238 <michaelester01@gmail.com>
2025-03-25 18:44:48 -0600

    refactored home page and added code for threads to be added.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,59 +1,119 @@
-import React from 'react';
-import {Row, Col, Card, Container} from 'react-bootstrap';
+import React, { useState } from'react';
+import { Row, Col, Card, Container } from'react-bootstrap';
 
+interface Thread {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  genre: string;
+  color: string;
+  url: string;
+}
 
-const Home: React.FC= () => {
-return (
-<><header style={{ paddingLeft: 0 }}></header>
+const threadsData: Thread[] = [
+    {
+        id: 1,
+        title: 'Action/Adventure Games',
+        content: 'Discuss your favorite action/adventure games here!',
+        createdAt: '2022-01-01',
+        genre: 'Action/Adventure',
+        color: '#FF9900',
+        url: '/threads/1',
+      },
+      {
+        id: 2,
+        title: 'Role-Playing Games (RPGs)',
+        content: 'Talk about your favorite RPGs and share your experiences!',
+        createdAt: '2022-01-02',
+        genre: 'RPG',
+        color: '#0099CC',
+        url: '/threads/2',
+      },
+      {
+        id: 3,
+        title: 'First-Person Shooter (FPS) Games',
+        content: 'Discuss your favorite FPS games and strategies!',
+        createdAt: '2022-01-03',
+        genre: 'FPS',
+        color: '#FF0000',
+        url: '/threads/3',
+      },
+      {
+        id: 4,
+        title: 'Sports Games',
+        content: 'Talk about your favorite sports games and teams!',
+        createdAt: '2022-01-04',
+        genre: 'Sports',
+        color: '#00CC00',
+        url: '/threads/4',
+      },
+      {
+        id: 5,
+        title: 'Strategy Games',
+        content: 'Discuss your favorite strategy games and tactics!',
+        createdAt: '2022-01-05',
+        genre: 'Strategy',
+        color: '#CCCC00',
+        url: '/threads/5',
+      },
+      {
+        id: 6,
+        title: 'Simulation Games',
+        content: 'Talk about your favorite simulation games and experiences!',
+        createdAt: '2022-01-06',
+        genre: 'Simulation',
+        color: '#6600CC',
+        url: '/threads/6',
+      },
+];
 
-<div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-                    <div className='d-flex justify-content-center align-items-center h-100'>
-                        <div className='text-white'>
-                            <div className="contact">
-                                <header className="contact-header">
-                                    <Container>
-                                        <Row>
-                                            <Col>
-                                                <Card.Body>
-                                                    <Card.Title>
-                                                        <h1 className="titel">Welcome To The Old Internet</h1>
-                                                    </Card.Title>
-                                                    </Card.Body>
-                                                </Col>
-                                            </Row>
-                                        </Container>
-                                    </header>
-                                </div>
-                            </div>
-                        </div>
+const Home: React.FC = () => {
+  const [selectedThread, setSelectedThread] = useState<Thread | null>(null);
 
-            <div className="container">
-            <div className="row">
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
+  const handleThreadClick = (thread: Thread) => {
+    setSelectedThread(thread);
+  };
 
-                // Force next columns to break to new line at md breakpoint and up
-                <div className="w-100 d-none d-md-block"></div>
-
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-
-                <div className="w-100 d-none d-md-block"></div>
-
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-                <div className="col-6 col-sm-4">.col-6 .col-sm-4</div>
-
-            </div>
-            </div>
-</div></> 
-
-
-);
+  return (
+    <Container fluid>
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <h1 className="title">Welcome To The Old Internet</h1>
+        </Col>
+      </Row>
+      <Row>
+        {threadsData.map((thread) => (
+          <Col key={thread.id} xs={6} sm={4} md={3} lg={2}>
+            <Card style={{ backgroundColor: thread.color, borderColor: thread.color }}>
+              <Card.Body>
+                <Card.Title style={{ color: 'white' }}>
+                  <a href="#" onClick={() => handleThreadClick(thread)}>
+                    {thread.title}
+                  </a>
+                </Card.Title>
+                <Card.Text style={{ color: 'white' }}>{thread.content.substring(0, 100)}...</Card.Text>
+                <Card.Text style={{ color: 'white' }}>Created at: {thread.createdAt}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      {selectedThread && (
+        <Row>
+          <Col xs={12}>
+            <Card>
+              <Card.Body>
+                <Card.Title style={{ color: 'white' }}>{selectedThread.title}</Card.Title>
+                <Card.Text style={{ color: 'white' }}>{selectedThread.content}</Card.Text>
+                <Card.Text style={{ color: 'white' }}>Created at: {selectedThread.createdAt}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </Container>
+  );
 };
+
 export default Home;
